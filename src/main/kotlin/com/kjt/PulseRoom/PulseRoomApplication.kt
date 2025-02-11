66717bc657edcfd3a8ac7e4ae738a173b75b6379
KangJeongTaek <dfgddjd@naver.com>
2025-02-11 09:39:13 +0900

    init project

---
+++ src/main/kotlin/com/kjt/PulseRoom/PulseRoomApplication.kt
@@ -0,0 +1,11 @@
+package com.kjt.PulseRoom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PulseRoomApplication
+
+fun main(args: Array<String>) {
+	runApplication<PulseRoomApplication>(*args)
+}
